38c837a096c6675c5ed9d102637df70f140edbf3
MusfiqDehan <musfiqdehan@gmail.com>
2025-12-02 19:10:46 +0600

    feat: Add django_celery_beat to INSTALLED_APPS for task scheduling support

--- backend/project_config/settings/base.py
+++ backend/project_config/settings/base.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "django_celery_beat",
 
     # Custom apps
     "apps.authentication",
