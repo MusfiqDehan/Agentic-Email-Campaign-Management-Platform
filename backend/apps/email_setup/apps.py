af5c81d0f17ef35dbd29fbef98a86beda39ac862
MusfiqDehan <musfiqdehan@gmail.com>
2025-12-02 18:16:37 +0600

    Remove email_setup URL configuration file to streamline routing and improve maintainability.

--- backend/apps/email_setup/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class NotifyHubConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'automation_rule'
-
-    def ready(self):  # pragma: no cover - side-effect import
-        from . import ses_event_handlers  # noqa: F401
