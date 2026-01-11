e77a60c7163d2a9655b925b329c0eac4ac143744
MusfiqDehan <musfiqdehan@gmail.com>
2026-01-11 19:49:56 +0600

    feat: enhance organization logo handling and add image optimization settings

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8001',
+        pathname: '/media/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
